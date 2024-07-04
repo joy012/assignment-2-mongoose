0ee7800217cda85030064e9b8ab8718e8d4f87de
joy012 <joy.buet.012@gmail.com>
2024-07-05 01:34:05 +0600

    route not found error handler implemented

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import router from './app/routes';
 
 export const app: Application = express();
@@ -8,3 +8,11 @@ app.use(express.json());
 app.use(cors);
 
 app.use('/api', router);
+
+// not found error handle
+app.use((req: Request, res: Response) => {
+  return res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
