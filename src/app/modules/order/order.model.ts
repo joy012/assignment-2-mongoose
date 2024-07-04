bd714649a7ae10897e8c4cd28a165af57d4ac51b
joy012 <joy.buet.012@gmail.com>
2024-07-05 01:35:05 +0600

    order routes, interface and model created

---
+++ src/app/modules/order/order.model.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose';
+import { IOrder } from './order.interface';
+
+const orderSchema = new Schema<IOrder>({
+  email: {
+    type: String,
+    required: [true, 'User email is required in order'],
+  },
+  productId: {
+    type: Schema.Types.ObjectId,
+    required: [true, 'Product id is required in order'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Product price is required in order'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'Product quantity is required in order'],
+  },
+});
+
+export const Order = model<IOrder>('Order', orderSchema);
